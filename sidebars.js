f2add7f6c8c0fa4dc598af02010b4e6d70f56846
polarcaptain <polarcapt1n@gmail.com>
2025-05-03 14:14:53 -0700

    a

--- sidebars.js
+++ sidebars.js
@@ -1,46 +1,23 @@
 // @ts-check
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
-/**
- * Creating a sidebar enables you to:
- - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
-
- The sidebars can be generated from the filesystem, or explicitly defined here.
-
- Create as many sidebars as you want.
-
- @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
- */
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   wikiSidebar: [
     {
-      type: 'doc',
-      id: 'intro',
-      label: 'Introduction to Psychometrics',
+      type: 'category',
+      label: 'Introduction',
+      link: {
+        type: 'doc',
+        id: 'intro',
+      },
+      collapsed: false,
+      items: [
+        
+      ],
     },
-    {
-      type: 'doc',
-      id: 'g_factor',
-      label: 'Rundown of the g factor',
-    },
-    {
-      type: 'doc',
-      id: 'faq',
-      label: 'Frequently Asked Questions',
-    },
-    {
-      type: 'doc',
-      id: 'resources',
-      label: 'Resources',
-    },
-    {
-      type: 'doc',
-      id: 'glossary',
-      label: 'Glossary',
-    },
+    'faq',
+    'resources',
+    'glossary',
+    'g_factor',
   ],
 };
 
